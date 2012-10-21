e11ca87fed7a87a8279a4707a2b76f363685d028
hunlan <linhunlan@gmail.com>
2012-10-20 18:07:14 -0700

    [God Sort] Implemented binary tree sort

--- sectionproject/uploadAndPrint/Sorter.py
+++ sectionproject/uploadAndPrint/Sorter.py
@@ -6,7 +6,11 @@ Created on Oct 18, 2012
 from heapq import heappush, heappop
 
 def GodSort(urls):
-    return urls
+    node = Node(urls.pop())
+    for url in urls:
+        node.insert(url)
+        
+    return node.getSorted()
 
 def MergeSort(urls):
     if len(urls) <= 1:
@@ -60,3 +64,39 @@ def _merge(left, right):
         j += 1
             
     return sort
+
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+    
+    def getLeft(self):
+        return self.left
+    
+    def getRight(self):
+        return self.right
+    
+    def getData(self):
+        return self.data
+    
+    def insert(self, data):
+        if data < self.data:
+            if self.left is None:
+                self.left = Node(data)
+            else:
+                self.left.insert(data)
+        else:
+            if self.right is None:
+                self.right = Node(data)
+            else:
+                self.right.insert(data)
+                
+    def getSorted(self):
+        ret = []
+        if self.left:
+            ret.extend(self.left.getSorted())
+        ret.append(self.data)
+        if self.right:
+            ret.extend(self.right.getSorted())
+        return ret
